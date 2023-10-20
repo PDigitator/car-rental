2a15171a80b59e2619db98ccfd1b5c510f174728
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-10-21 01:34:37 +0300

    Refactored Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,37 +2,76 @@ import PropTypes from 'prop-types';
 
 import {
   Btn,
-  BtnLearn,
   BtnLoad,
+  BtnLearn,
   BtnSearch,
 } from 'components/Button/Button.styled';
 
 const Button = ({ name, type, text, onClick }) => {
-  return (
-    <>
-      {name === 'learn' ? (
+  let BtnComponent;
+
+  switch (name) {
+    case 'load':
+      BtnComponent = (
         <BtnLearn type={type} onClick={onClick} aria-label="Learn more">
           {text && { text }}
         </BtnLearn>
-      ) : name === 'load' ? (
+      );
+      break;
+
+    case 'learn':
+      BtnComponent = (
         <BtnLoad type={type} onClick={onClick} aria-label="Load more">
           {text && { text }}
         </BtnLoad>
-      ) : name === 'search' ? (
+      );
+      break;
+
+    case 'search':
+      BtnComponent = (
         <BtnSearch type={type} onClick={onClick} aria-label="Search">
           {text && { text }}
         </BtnSearch>
-      ) : (
+      );
+      break;
+
+    default:
+      BtnComponent = (
         <Btn type={type} onClick={onClick}>
           {text && { text }}
         </Btn>
-      )}
-    </>
-  );
+      );
+      break;
+  }
+
+  return <>{BtnComponent}</>;
+
+  // return (
+  //   <>
+  //     {name === 'load' ? (
+  //       <BtnLearn type={type} onClick={onClick} aria-label="Learn more">
+  //         {text && { text }}
+  //       </BtnLearn>
+  //     ) : name === 'learn' ? (
+  //       <BtnLoad type={type} onClick={onClick} aria-label="Load more">
+  //         {text && { text }}
+  //       </BtnLoad>
+  //     ) : name === 'search' ? (
+  //       <BtnSearch type={type} onClick={onClick} aria-label="Search">
+  //         {text && { text }}
+  //       </BtnSearch>
+  //     ) : (
+  //       <Btn type={type} onClick={onClick}>
+  //         {text && { text }}
+  //       </Btn>
+  //     )}
+  //   </>
+  // ); //!
 };
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   onClick: PropTypes.func,
 };
--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -52,8 +52,8 @@ const BtnLearn = styled(Btn)`
   &:hover,
   &:focus {
     background-color: ${staticTheme.colors.accentHover};
-    box-shadow: 4px 1px 4px rgba(0, 0, 0, 0.12), 0 4px 4px rgba(0, 0, 0, 0.06),
-      1px 4px 6px rgba(0, 0, 0, 0.16);
+    box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+      0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
   }
 `;
 
